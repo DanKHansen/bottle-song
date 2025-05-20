5bf1202b67f23f31495e1d9b9ab2c8256498cbcf
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-20 12:49:39 +0200

    refactor

--- src/main/scala/BottleSong.scala
+++ src/main/scala/BottleSong.scala
@@ -1,23 +1,8 @@
 object BottleSong:
-   private val m = Map(
-     10 -> "Ten",
-     9 -> "Nine",
-     8 -> "Eight",
-     7 -> "Seven",
-     6 -> "Six",
-     5 -> "Five",
-     4 -> "Four",
-     3 -> "Three",
-     2 -> "Two",
-     1 -> "One",
-     0 -> "No")
-   def recite(startBottles: Int, takeDown: Int): String =
-      val song =
-         for s <- startBottles until startBottles - takeDown by -1
-         yield s"${m(s)} green ${if s == 1 then "bottle" else "bottles"} hanging on the wall,\n" +
-            s"${m(s)} green ${if s == 1 then "bottle" else "bottles"} hanging on the wall,\n" +
-            s"And if one green bottle should accidentally fall,\n" +
-            s"There'll be ${m(s - 1).toLowerCase} green ${
-                  if s - 1 == 1 then "bottle" else "bottles"
-               } hanging on the wall.\n"
-      song.mkString("\n")
+   private val l = Seq("No", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten")
+   def recite(s: Int, t: Int): String =
+     (for b <- s until s - t by -1
+        yield s"${l(b)} green bottle${if b == 1 then "" else "s"} hanging on the wall,\n" * 2 +
+              s"And if one green bottle should accidentally fall,\n" +
+              s"There'll be ${l(b - 1).toLowerCase} green bottle${if b - 1 == 1 then "" else "s"} hanging on the wall.\n")
+     .mkString("\n")
